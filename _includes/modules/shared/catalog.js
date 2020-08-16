bfddaf34f45ba478c4d04a2ba1601bd3a49b9f8d
JD <jd@izorian.co.uk>
2020-08-16 13:35:10 +0000

    Site v0.0.4e

--- _includes/modules/shared/catalog.js
+++ _includes/modules/shared/catalog.js
@@ -28,6 +28,8 @@ Catalog = (options, factory) => {
   
   var _run = (query, db) => (factory.Flags.log("Running QUERY:", query), (db || ರ‿ರ.db).exec(query));
   
+  var _name = (key, value) => value.name || key.toUpperCase();
+  
   var _index = (name, data) => data.columns.indexOf(name) >= 0 ? data.columns.indexOf(name) : false;
   
   var _columns = (columns, data, optional) => {
@@ -51,6 +53,18 @@ Catalog = (options, factory) => {
       /* <!-- Process Dates --> */
       if (date_columns && date_columns.length > 0) _.each(_columns(date_columns, data), column => _.each(data.values, row => row[column] = row[column] ? factory.Dates.parse(row[column]) : row[column]));
 
+      /* <!-- Process Custom Columns --> */
+      if (ರ‿ರ.custom) {
+        
+        /* <!-- Process Enums / Formats --> */
+        _.each(["enums", "format"], fn => _.chain(ರ‿ರ.custom).pick(value => value[fn]).each((value, key) => {
+          var column = _index(_name(key, value), data);
+          if (column !== false) _.each(data.values, row => row[column] = row[column] ? 
+            _.isArray(row[column]) ? _.map(row[column], value[fn]) : value[fn](row[column]) : row[column]);
+        }));
+        
+      }
+      
       /* <!-- Clear Empty Columns --> */
       var _all = _.range(data.columns.length);
        _.each(data.values, row => {
@@ -67,6 +81,39 @@ Catalog = (options, factory) => {
     }
     
     return data;
+  };
+  
+  var _parse = {
+    
+    enums : details => {
+      var _match = details.match(/^ENUMS:?=?(.+)/i);
+      if (_match && _match[1]) {
+        var _enums = _.reduce(_match[1].split(";"), (memo, value) => {
+          value = value.split("=");
+          memo[value[0]] = value[1];
+          return memo;
+        }, {});
+        return value => _enums[value] ? {
+          value: value,
+          text: _enums[value],
+        } : value;
+      }  
+    },
+    
+    format : details => {
+      var _match = details.match(/^DISPLAY_FORMAT:?=?(.+)/i);
+      if (_match && _match[1]) {
+        if (/^HTTPS?:\/\/.+/i.test(_match[1])) {
+          return value => ({
+            text: value,
+            url: _match[1].replace(/\{\{value\}\}/i, value)
+          });
+        } else {
+          return value => _match[1].replace(/\{\{value\}\}/i, value);
+        }
+      }
+    },
+    
   };
   
   var _interrogate = db => {
@@ -90,18 +137,41 @@ Catalog = (options, factory) => {
     }
     if (ರ‿ರ.tables.indexOf("custom_columns") >= 0) {
       var _custom = _data(_run("SELECT * FROM custom_columns WHERE mark_for_delete = 0;")),
-          _custom_cols = _columns(["id", "label", "name", "datatype", "is_multiple", "normalized"], _custom);
+          _custom_cols = _columns(["id", "label", "name", "datatype", "is_multiple", "normalized", "display"], _custom);
       if (_custom_cols) {
         ರ‿ರ.custom = _.reduce(_custom.values, (memo, value) => {
-          memo[value[_custom_cols.label]] = {
+          
+          var _custom_column = {
             id : value[_custom_cols.id],
             name : value[_custom_cols.name],
             type : value[_custom_cols.datatype],
             multiple : value[_custom_cols.is_multiple],
             normalised : value[_custom_cols.normalized],
+            display : value[_custom_cols.display] ? JSON.parse(value[_custom_cols.display]) : null,
             table : _exists(`custom_column_${value[_custom_cols.id]}`, ರ‿ರ.tables),
             link_table : _exists(`books_custom_column_${value[_custom_cols.id]}_link`, ರ‿ರ.tables),
           };
+          
+          if (_custom_column.display && _custom_column.display.description)
+            _custom_column.description = _custom_column.display.description.split("|");
+          
+          if (_custom_column.description) {
+            
+            /* <!-- Clean Up Descriptions --> */
+            _custom_column.description = _.chain(_custom_column.description).map(value => value.trim()).compact().value();
+            
+            /* <!-- Check for Public Flag --> */
+            _custom_column.public = !!_.find(_custom_column.description, value => /PUBLIC/i.test(value));
+            
+            /* <!-- Check for Display Format --> */
+            _custom_column.format = _.reduce(_custom_column.description, (fn, details) => fn || _parse.format(details), null);
+            
+            /* <!-- Check for ENUMS --> */
+            _custom_column.enums = _.reduce(_custom_column.description, (fn, details) => fn || _parse.enums(details), null);
+            
+          }
+          
+          memo[value[_custom_cols.label]] = _custom_column;
           return memo;
         }, {});
         factory.Flags.log("META [Custom Columns]:", ರ‿ರ.custom);
@@ -116,16 +186,23 @@ Catalog = (options, factory) => {
   var _builders = {
     
     custom : {
-    
-      select : (properties, separator) => _.reduce(properties, (memo, value, key) => {
-        if (value.table && value.link_table) memo.push(`(SELECT ${value.multiple ? `GROUP_CONCAT(value, '${separator || "\n"}')` : "value"} FROM ${value.table} WHERE ${value.table}.id IN (SELECT value from ${value.link_table} WHERE book = books.id)) ${key.toUpperCase()},`);
+      
+      select : (properties, separator, public_only) => _.reduce(properties, (memo, value, key) => {
+        if (!public_only || value.public) {
+          if (value.table && value.link_table) {
+            memo.push(`(SELECT ${value.multiple ? `GROUP_CONCAT(value, '${separator || "\n"}')` : "value"} FROM ${value.table} WHERE ${value.table}.id IN (SELECT value from ${value.link_table} WHERE book = books.id)) "${_name(key, value)}",`);
+          } else if (value.table) {
+            memo.push(`(SELECT value FROM ${value.table} WHERE book = books.id) "${_name(key, value)}",`);
+          }  
+        }
         return memo;
       }, []).join("\n"),
       
-      search : (properties, terms) => _.reduce(properties, (memo, value, key) => {
-        memo.push(` or ${key.toUpperCase()} like '%${terms}%'`);
+      search : (properties, terms, public_only) => _.reduce(properties, (memo, value, key) => {
+        if ((!public_only || value.public) && value.type == "text") memo.push(` or "${_name(key, value)}" like '%${terms}%'`);
         return memo;
       }, []).join(""),
+      
     },
     
     identifiers : {
@@ -138,7 +215,7 @@ Catalog = (options, factory) => {
       
       search : (identifiers, terms) => _.reduce(identifiers, (memo, type) => {
         if (type && type.indexOf("isbn") >= 0) 
-          memo.push(` or ${type.toUpperCase()} like '%${terms}%'`);
+          memo.push(` or "${type.toUpperCase()}" like '%${terms}%'`);
         return memo;
       }, []).join(""),
       
@@ -169,16 +246,16 @@ Catalog = (options, factory) => {
     search : {
       
       books : terms => _process(["Authors", "Tags"].concat(ರ‿ರ.custom ? _.reduce(ರ‿ರ.custom, (memo, value, key) => {
-        if (value.multiple) memo.push(key.toUpperCase());
+        if (value.public && value.multiple) memo.push(_name(key, value));
         return memo;
       }, []) : []), null, _data(_run(_.compact([
         "SELECT id ID, title Title,",
         "(SELECT GROUP_CONCAT(name, '\n') FROM books_authors_link AS bal JOIN authors ON(author = authors.id) WHERE book = books.id) Authors,",
         ರ‿ರ.identifiers ? _builders.identifiers.select(ರ‿ರ.identifiers) : "",
-        ರ‿ರ.custom ? _builders.custom.select(ರ‿ರ.custom) : "",
+        ರ‿ರ.custom ? _builders.custom.select(ರ‿ರ.custom, null, true) : "",
         "(SELECT GROUP_CONCAT(name, '\n') FROM series WHERE series.id IN (SELECT series from books_series_link WHERE book = books.id)) Series,",
         "(SELECT GROUP_CONCAT(name, '\n') FROM tags WHERE tags.id IN (SELECT tag from books_tags_link WHERE book = books.id)) Tags",
-        `FROM books WHERE Title like '%${terms}%' or Authors like '%${terms}%' or Tags like '%${terms}%'${ರ‿ರ.identifiers ? _builders.identifiers.search(ರ‿ರ.identifiers, terms) : ""}${ರ‿ರ.custom ? _builders.custom.search(ರ‿ರ.custom, terms) : ""}`
+        `FROM books WHERE Title like '%${terms}%' or Authors like '%${terms}%' or Tags like '%${terms}%'${ರ‿ರ.identifiers ? _builders.identifiers.search(ರ‿ರ.identifiers, terms) : ""}${ರ‿ರ.custom ? _builders.custom.search(ರ‿ರ.custom, terms, true) : ""}`
       ]).join("\n")))),
       
     },
@@ -186,7 +263,7 @@ Catalog = (options, factory) => {
     find : {
       
       book : id => _process(["Authors", "Tags"].concat(ರ‿ರ.custom ? _.reduce(ರ‿ರ.custom, (memo, value, key) => {
-        if (value.multiple) memo.push(key.toUpperCase());
+        if (value.multiple) memo.push(_name(key, value));
         return memo;
       }, []) : []), ["Published", "Modified"], _data(_run(_.compact([
         "SELECT id ID, title Title, pubdate Published, path Path, has_Cover Cover, last_modified Modified,",
